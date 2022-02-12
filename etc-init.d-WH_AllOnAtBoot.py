461ba789731c28afd3d1cc72329cc2df5ae55d23
paulobrecht <paul.obrecht+github@gmail.com>
2022-02-12 14:11:17 -0700

    paste of WH_AllOnAtBoot rather than ln -s, which didn't work for a root:root file

--- etc-init.d-WH_AllOnAtBoot.py
+++ etc-init.d-WH_AllOnAtBoot.py
@@ -1,1 +1,38 @@
-/etc/init.d/WH_AllOnAtBoot.py
+#!/usr/bin/python3
+### BEGIN INIT INFO
+# Provides:          WH_AllOnAtBoot.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Run once at boot time to turn on all electrical panel relays
+# Description:       Turns on all electrical panel relays at boot (which are later controlled by cron)
+### END INIT INFO
+
+import RPi.GPIO as gpio
+
+#################################
+# Write line to log file, default with timestamp
+#################################
+
+def logFunc(logloc="/home/pi/CurbAPI/activity_log.txt", line, now = None):
+  from time import strftime, localtime
+  log = open(logloc, "a")
+  now = strftime("%H:%M:%S", localtime())
+  log.write(now + ": " + line + "\n")
+  log.close()
+
+# Water heater GPIO setup
+WH_north, WH_south, ppump = [11, 13, 15] # board pins
+gpio.setmode(gpio.BOARD)
+gpio.setwarnings(False)
+gpio.setup(WH_north,gpio.OUT)
+gpio.setup(WH_south,gpio.OUT)
+gpio.setup(ppump,gpio.OUT)
+
+# Turn on all at bootup
+gpio.output(WH_north,1)
+gpio.output(WH_south,1)
+gpio.output(ppump,1)
+
+logfunc("pi-wh.lan rebooted. Restarting now with (1, 1, 1)")
