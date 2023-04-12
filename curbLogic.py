62176872c6d7da77ceee3be9fe13e7b99bb0d47b
paulobrecht <paul.obrecht+github@gmail.com>
2023-04-12 14:36:17 -0700

    Added low-production logic for school hours on weekdays

--- curbLogic.py
+++ curbLogic.py
@@ -20,9 +20,12 @@ try:
 	first_json_timestamp = usage["timestamp"]
 	data_lag = int(round(mktime(localtime()) - usage["timestamp"]))
 	WHS, WHN, DRY, HPS, HPN, SUB, PP, totalHogConsumption = LF.curbUsage(usage)
+	PRD = abs(usage['production'])
 except BaseException:
 	LF.logFunc(logloc = logloc, line = "ERROR: Problems reading/parsing consumption JSON file.")
 	sys.exit()
+
+
 
 ############
 # Curb Logic
@@ -32,6 +35,12 @@ except BaseException:
 initial_dict = LF.gpioCheckStatus(["WH_north", "WH_south"])
 WHN_status = [initial_dict["WH_north"]] # start list that will be [before, after]
 WHS_status = [initial_dict["WH_south"]] # start list that will be [before, after]
+
+# for production-based override
+isSchoolHours = int(strftime("%H",localtime())) in range(6,14)
+isWeekday = int(strftime("%w",localtime())) in range(1,6)
+month = int(strftime("%m", localtime()))
+lowProduction = (month in range(3,11) and PRD < 5000) or (month in [1,2,11,12] and PRD < 4000)
 
 # status_message starts with lag indicator if there's a lag
 # status_message = "Curb: " + strftime("%H:%M:%S", localtime(first_json_timestamp)) + ", "
@@ -50,6 +59,11 @@ elif LF.isOn("DRY", DRY): # if dryer is on, turn both water heaters off
 
 elif LF.isOn("SUB", SUB): # if kitchen consumption is really high, turn both water heaters off
 	status_message += "Kitchen consumption very high (" + str(SUB) + " w), turning off both water heaters."
+	LF.gpioSetStatus(status_dict = {"WH_north": 0, "WH_south": 0})
+
+# don't let water heaters run during school hours on weekdays if production is low
+elif isWeekday and isSchoolHours and lowProduction:
+	status_message += "Low production (" + str(PRD) + " w), turning off both water heaters."
 	LF.gpioSetStatus(status_dict = {"WH_north": 0, "WH_south": 0})
 
 # if one water heater is on, turn the other off -- prioritize north because more showers are there
@@ -82,4 +96,4 @@ if any_change:
 	LF.logFunc(logloc=logloc, line=status_message)
 elif (WHN_status, WHS_status) != ([1, 1], [1, 1]):
 	tmp = {"HPN": HPN, "HPS": HPS, "WHN": WHN, "WHS": WHS, "DRY": DRY, "SUB": SUB, "PP": PP, "status N,S": (WHN_status, WHS_status)}
-	LF.logFunc(logloc=logloc, line="No changes. " + dumps(tmp))
+#	LF.logFunc(logloc=logloc, line="No changes. " + dumps(tmp))
